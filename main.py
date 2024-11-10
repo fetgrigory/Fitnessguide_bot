5a87685747f89b7f18f9416ace83f0626b9cccb0
fetgrigory <157891679+fetgrigory@users.noreply.github.com>
2024-11-10 20:56:59 +0300

    add FSMContext

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 # Installing the necessary libraries
 import os
 import datetime
-from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message, CallbackQuery
-from aiogram.filters import Command
+from aiogram import Bot, Dispatcher, types
 from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
 # from app.database.sqlite3_db import create_table, insert_workout_data, get_workout_data
 from app.database.PostgreSQL_db import create_table, insert_workout_data, get_workout_data
 from app.keyboards import main_keyboard
-import asyncio
-
-
+from dotenv import load_dotenv
+from aiogram.filters import Command
+from aiogram.types import CallbackQuery, Message
 # Load environment variables
 load_dotenv()
 
 # Initialization of the bot and dispatcher
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher(storage=MemoryStorage())
+
+
+# Define states
+class WorkoutStates(StatesGroup):
+    BenchPress = State()
+    DumbbellFly = State()
+    Situps = State()
+    Height = State()
+    Weight = State()
+
 
 # Global variable to store user data
 USER_DATA = {}
@@ -35,7 +45,6 @@ create_table()
 # Ensuring that the bot is running
 print('Бот успешно запущен!')
 
-
 @dp.message(Command("start"))
 async def start(message: Message):
     USER_DATA.clear()
@@ -45,22 +54,19 @@ async def start(message: Message):
                          f"Меня зовут {me.first_name}, Я помогу вам вести учет фитнес-тренировок.",
                          reply_markup=keyboard)
 
-
-# Begin asking questions to collect workout data
+# Handler for initiating the workout data collection process
 @dp.callback_query(lambda c: c.data == 'add_data')
-async def add_workout_data(callback_query: CallbackQuery):
-    await ask_next_question(callback_query.message)
-
-
+async def add_workout_data(callback_query: CallbackQuery, state: FSMContext):
+    await callback_query.answer()
+    await ask_question(callback_query.message, state, WorkoutStates.BenchPress, questions[0])
+# Handler to retrieve and display stored workout data
 @dp.callback_query(lambda c: c.data == 'get_data')
 async def get_workout_data_handler(callback_query: CallbackQuery):
     await callback_query.answer()
     data = get_workout_data()
     if not data:
-        # No workout data available
         await bot.send_message(callback_query.message.chat.id, "Данных о тренировках нет!")
         return
-        # Prepare a formatted string of workout data
     result = ''
     for record in data:
         result += (f"Дата: {record[1]}\n"
@@ -73,64 +79,90 @@ async def get_workout_data_handler(callback_query: CallbackQuery):
     await bot.send_message(callback_query.message.chat.id, f"Данные о тренировках:\n{result}")
 
 
-async def ask_next_question(message: Message):
-    # Ask the next question if all questions haven't been answered yet
-    if len(USER_DATA) < len(questions):
-        question = questions[len(USER_DATA)]
-        await message.answer(question)
-        USER_DATA['current_question'] = question
-    else:
-        # Save the user's workout data after all questions are answered
-        await save_workout_data(message)
+# Function to ask a question and set the next state
+async def ask_question(message: types.Message, state: FSMContext, next_state: State, question_text: str):
+    await message.answer(question_text)
+    await state.set_state(next_state)
 
 
-@dp.message()
-async def add_workout(message: Message):
-    answer = message.text
-    if 'current_question' in USER_DATA:
-        question_index = questions.index(USER_DATA['current_question'])
-        # Exercise counts need to be integers
-        if question_index in {0, 1, 2}:
-            if not answer.isdigit():
-                await message.answer("Пожалуйста, введите числовое значение.")
-                return
-            # Height and weight need to be floats
-        elif question_index in {3, 4}:
-            try:
-                float(answer)
-            except ValueError:
-                await message.answer("Пожалуйста, введите числовое значение.")
-                return
-# Store the answer and proceed to the next question
-        USER_DATA[USER_DATA['current_question']] = answer
-        del USER_DATA['current_question']
-        await ask_next_question(message)
+# Handler to process Bench Press data
+@dp.message(WorkoutStates.BenchPress)
+async def process_bench_press(message: types.Message, state: FSMContext):
+    if not message.text.isdigit():
+        await message.answer("Пожалуйста, введите числовое значение.")
+        return
+    await state.update_data(bench_press=int(message.text))
+    await ask_question(message, state, WorkoutStates.DumbbellFly, questions[1])
+# Handler to process Dumbbell Fly data
+@dp.message(WorkoutStates.DumbbellFly)
+async def process_dumbbell_fly(message: types.Message, state: FSMContext):
+    if not message.text.isdigit():
+        await message.answer("Пожалуйста, введите числовое значение.")
+        return
+    await state.update_data(dumbbell_fly=int(message.text))
+    await ask_question(message, state, WorkoutStates.Situps, questions[2])
 
 
-async def save_workout_data(message: Message):
-    # Current date and time for the record
+# Handler to process Situps data
+@dp.message(WorkoutStates.Situps)
+async def process_situps(message: types.Message, state: FSMContext):
+    if not message.text.isdigit():
+        await message.answer("Пожалуйста, введите числовое значение.")
+        return
+    await state.update_data(situps=int(message.text))
+    await ask_question(message, state, WorkoutStates.Height, questions[3])
+
+
+# Handler to process height data
+@dp.message(WorkoutStates.Height)
+async def process_height(message: types.Message, state: FSMContext):
+    try:
+        height = float(message.text)
+    except ValueError:
+        await message.answer("Пожалуйста, введите числовое значение.")
+        return
+    await state.update_data(height=height)
+    await ask_question(message, state, WorkoutStates.Weight, questions[4])
+
+
+# Handler to process weight data and calculate BMI
+@dp.message(WorkoutStates.Weight)
+async def process_weight(message: types.Message, state: FSMContext):
+    try:
+        weight = float(message.text)
+    except ValueError:
+        await message.answer("Пожалуйста, введите числовое значение.")
+        return
+    await state.update_data(weight=weight)
+
+    # Retrieve all user inputs
+    user_data = await state.get_data()
+
+    # Calculate BMI
+    height_m = user_data['height'] / 100
+    weight_kg = user_data['weight']
+    bmi, category = calculate_bmi_category(height_m, weight_kg)
+
+    # Prepare the data items list to store in the database
     current_date = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    # Convert to meters for BMI calculation
-    height = float(USER_DATA.get(questions[3], 0)) / 100
-    weight = float(USER_DATA.get(questions[4], 0))
-    bmi, category = calculate_bmi_category(height, weight)
-    # Prepare the data items list to store in database
     data = [
         current_date,
-        USER_DATA.get(questions[0], ""),
-        USER_DATA.get(questions[1], ""),
-        USER_DATA.get(questions[2], ""),
-        USER_DATA.get(questions[3], ""),
-        USER_DATA.get(questions[4], ""),
+        user_data['bench_press'],
+        user_data['dumbbell_fly'],
+        user_data['situps'],
+        user_data['height'],
+        user_data['weight'],
         bmi,
         category
     ]
     insert_workout_data(data)
     await message.answer(f"Данные о тренировке успешно сохранены!\nВаш ИМТ: {bmi:.2f} ({category})")
+    # Finish the state
+    await state.clear()
 
 
+# Calculate BMI
 def calculate_bmi_category(height_in_meters: float, weight_in_kg: float):
-    # Calculate BMI
     bmi = weight_in_kg / height_in_meters**2
     # Determine BMI category based on the calculated value
     if bmi <= 16:
@@ -153,5 +185,6 @@ def calculate_bmi_category(height_in_meters: float, weight_in_kg: float):
 async def main():
     # Start polling to keep the bot running and listening for messages
     await dp.start_polling(bot)
+
 if __name__ == '__main__':
     asyncio.run(main())
